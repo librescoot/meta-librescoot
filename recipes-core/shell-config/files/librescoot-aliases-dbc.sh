0728b8ce484761adb0ed04989a59ae89a0c03eda
Teal Bauer <teal@starsong.eu>
2025-11-12 14:36:04 +0100

    feat: alias redis-cli to default to MDB on DBC
    All redis-cli invocations on DBC now automatically connect to MDB
    without requiring explicit -h flag for each command.

--- recipes-core/shell-config/files/librescoot-aliases-dbc.sh
+++ recipes-core/shell-config/files/librescoot-aliases-dbc.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 # LibreScoot shell aliases for DBC
 
+# Default redis-cli to connect to MDB
+alias redis-cli='redis-cli -h mdb'
+
 # Redis CLI shortcuts (connect to MDB)
-alias hgetall='redis-cli -h mdb hgetall'
-alias hget='redis-cli -h mdb hget'
-alias hset='redis-cli -h mdb hset'
-alias hdel='redis-cli -h mdb hdel'
-alias publish='redis-cli -h mdb publish'
-alias lpush='redis-cli -h mdb lpush'
-alias lrange='redis-cli -h mdb lrange'
-alias keys='redis-cli -h mdb keys'
+alias hgetall='redis-cli hgetall'
+alias hget='redis-cli hget'
+alias hset='redis-cli hset'
+alias hdel='redis-cli hdel'
+alias publish='redis-cli publish'
+alias lpush='redis-cli lpush'
+alias lrange='redis-cli lrange'
+alias keys='redis-cli keys'
 
 # SSH shortcuts
 alias mdb='ssh root@mdb'
