bf6d8ef6e8daddd456d04e9376cc67644cd2369b
Danylo Storozhev <office@storozhev.net>
2026-08-03 08:20:33 +0200

    feat(dropbear): enable legacy ssh-dss support

--- recipes-core/dropbear/files/localoptions.h
+++ recipes-core/dropbear/files/localoptions.h
@@ -3,3 +3,6 @@
    enabled so upgraded and non-upgraded boards can still ssh to each
    other (the installer trampoline flashes the DBC over this link). */
 #define DROPBEAR_RSA_SHA1 1
+
+/* Legacy ssh-dss host/user keys, disabled by default upstream. */
+#define DROPBEAR_DSS 1
